cd6a2e25625bafbce1858de10fe79874bb12f69e
joeCodes05 <nathancodes05@gmail.com>
2023-10-17 10:00:19 +0100

    file renaming

--- config/database.config.js
+++ config/database.config.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const database = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "splash_database"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 database.connect((err) => {
-  if(err) {
+  if (err) {
     console.log(`Error connecting to database`, err);
   } else {
     // create service table
@@ -20,7 +21,7 @@ database.connect((err) => {
       create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     ) CHARSET utf8mb4 COLLATE = utf8mb4_unicode_ci, ENGINE = InnoDB`);
-    console.log("Connection to database successfull");
+    console.log("Connection to database successful");
   }
 });
 
