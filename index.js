06b00e2b2472c49fb4d007cbab7f3aa45e1d3783
joeCodes05 <nathancodes05@gmail.com>
2023-10-31 19:55:42 -1100

    code update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 
+// routes
+const authRoutes = require("./routes/auth/auth.routes");
+
 const app = express();
 app.use(cors("*"));
+
+app.use("/api/auth", authRoutes);
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cors("*", corsOptions));
+app.use(express.json({ urlencoded: 30867, limit: "25mb", extended: true }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
